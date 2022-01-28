16c75bcbcdbec0dced9537db47d84c1775fc1772
iajordan <sp4c2d0ut@gmail.com>
2022-01-27 17:24:35 -0800

    Modified the test cases for part d.

--- lab1.c
+++ lab1.c
@@ -1,7 +1,12 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+
+#include "stddef.h"
+
+
 int exit_test(void);
+int waitpid_test(void);
 
 int main(int argc, char *argv[]){
 	switch(*argv[1]){
@@ -16,7 +21,11 @@ int main(int argc, char *argv[]){
 			printf(1,"\nSecond test\n");
 			break;
 		case '3':
-			printf(1,"\nThird Test\n");
+			printf(1,"\nTesting waitpid(pid,status,options) for Part c.\n");
+			waitpid_test();
+			break;
+		case '4':
+			printf(1,"\nTesting Bonus for Part e.\n");
 
 			break;
 	}
@@ -37,5 +46,37 @@ int exit_test(void){
 	}
 	pid= wait();
 	printf(1, "Child has terminated \n");
+	return 0;
+}
+
+int waitpid_test(void){
+	int pid, status;
+
+	char *argv[3] = {"./echo", "\"ECHO TEST\"", NULL};
+
+	printf(1,"Hello from the Parent proccess. PID: %d\n", getpid());
+
+	pid = fork();
+	if (pid < 0){
+		printf(1,"Error iwth fork()\n");
+		exitS(-1);
+	}
+	else if (pid == 0){
+		printf(1,"Hello from the child proccess. PID: %d\n", getpid());
+		exec(argv[0], argv);
+		exitS(1);
+	}
+	else{
+		printf(1,"Hello from the Parent proccess. PID: %d\n", getpid());
+		waitpid(pid, &status, 0);
+		// if (WIFEXITED(status)){
+		// 	printf(1,"Child ( %d ) terminated normally\n", pid); 
+		// }
+		// else if (WIFSIGNALED(status)){
+		// 	printf(1,"Child ( %d ) terminated by signal\n", pid); 
+		// }
+		printf(1,"Child ( %d ) terminated normally\n", pid); 
+
+	}
 	return 0;
 }
